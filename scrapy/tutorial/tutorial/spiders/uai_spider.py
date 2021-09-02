616e57733d25822da186eabbc101b70d7350d065
douglasmg7 <douglasmg7@gmail.com>
2021-09-02 01:57:42 -0300

    new select

--- scrapy/tutorial/tutorial/spiders/uai_spider.py
+++ scrapy/tutorial/tutorial/spiders/uai_spider.py
@@ -5,13 +5,21 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://www.uai.com.br',
+            #  'http://www.uai.com.br',
+            'file:///home/douglasmg7/code/python/scrapy/tutorial/uai.html'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        filename = 'uai.html'
+        filename = 'uai_temp.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
+            #  l = response.css('span.news::text').getall()
+            #  len(response.css('span.news.-title').getall())
+            #  >>> l =response.css('span.news.-title::text').getall()
+            #  >>> [x for x in l if 'Bolsa' in x]
+            #  [x for x in l if re.search(r'\bBolsa\b', x)]
+            #  [x for x in l if re.search(r'\bbolsa\b', x, flags=re.IGNORECASE)]
+
         self.log(f'Saved file {filename}')
