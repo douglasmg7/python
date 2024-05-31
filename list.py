cfc173cc17eba7c848c177578cb0343eca509f8c
douglasmg7 <douglasmg7@gmail.com>
2024-05-31 16:56:55 -0300

    play with list

--- list.py
+++ list.py
@@ -6,3 +6,16 @@ print('Last', states[-1])
 # Print all states.
 for state in states:
     print(state)
+
+st = states
+states.append('pa')
+print(states)
+print(st)
+
+# Point to same reference.
+print(id(states))
+print(id(st))
+
+# Remove some itens.
+states[2:4] = []
+print(states)
