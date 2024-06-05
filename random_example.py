36232a00c67ba390b0d4510488e8f837d0c944e4
douglasmg7 <douglasmg7@gmail.com>
2024-06-05 10:07:38 -0300

    random and string methods

---
+++ random_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import random
+
+l=list('asdfqwerzxcv')
+print('List: ', l)
+#  print(random.random())
+print('choice: ', random.choice(l))
+
+#  print(l)
--- string.py
+++ string.py
@@ -13,3 +13,39 @@ print(s)
 s = 'python'
 print(s[0])
 print(s[0:3])
+s2 = s[0:3]
+print('s2: ', s2)
+s = '111111111111'
+print('s1: ', s)
+print('s2: ', s2)
+
+# String to list -> list to string.
+s='olá mundo'
+l=list(s)
+print('l:', l)
+l[0] = 'O'
+print('l:', l)
+s=''.join(l) + '!'
+print('s: ', s)
+
+# Find.
+print(s.find('mu'))
+
+# Replace:
+print(s.replace('mundo', 'world'))
+
+# Split:
+print('split: ', s.split(' '))
+
+print('isalpha: ', s.isalpha())
+print('isdigit: ', s.isdigit())
+
+# Upper case:
+print('upper: ', s.upper())
+
+# Format:
+s = '%s e %s' % ('plantas', 'formigas')
+print(s)
+
+s = '{} e {}'.format('plantas', 'formigas')
+print(s)
