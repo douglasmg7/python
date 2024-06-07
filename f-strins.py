30c3a5b931f4782dcd5f9d62ba8df5c5f7e14d21
douglasmg7 <douglasmg7@gmail.com>
2024-06-07 11:44:47 -0300

    pytho f-strings

---
+++ f-strins.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#  f_name='douglas'
+#  m_name='maximiliano'
+#  l_name='gomes'
+
+#  f_name, m_name, l_name = ['douglas', 'maximiliano', 'gomes']
+
+f_name, m_name, l_name = 'douglas', 'maximiliano', 'gomes'
+
+print(f'{f_name} {m_name} {l_name}'.title())
